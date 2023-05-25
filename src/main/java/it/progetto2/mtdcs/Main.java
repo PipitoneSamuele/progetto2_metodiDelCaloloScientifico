fc56a712f2807dc8658b255e2cd8b154eb49e026
PipitoneSamuele <samuele.pipitone@gmail.com>
2023-05-25 13:00:17 +0200

    caricamento immagine funzionante

--- src/main/java/it/progetto2/mtdcs/Main.java
+++ src/main/java/it/progetto2/mtdcs/Main.java
@@ -12,72 +12,82 @@ import java.io.IOException;
 
 public class Main {
 
+    static String imagePath = "";
+    static JFrame frame;
+    static JPanel mplCenter;
+
     public static void main(String[] args) throws IOException {
 
-        /*
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                /*
           Finestra principale
          */
-        JFrame frame = GUI_Handler.initMainFrame();
+                frame = GUI_Handler.initMainFrame();
 
-        /*
-         * Pannello superiore
-        */
-        JPanel topPanel = GUI_Handler.createPanel(Color.green, 0,0,1200,100);
+                /*
+                 * Pannello superiore
+                 */
+                JPanel topPanel = GUI_Handler.createPanel(Color.green, 0,0,1200,100);
 
-        /*
-         * Pannello centro sinistra
-        */
-        JLayeredPane midPanelLeft = new JLayeredPane();
-        midPanelLeft.setBounds(0,100,600,600);
+                /*
+                 * Pannello centro sinistra
+                 */
+                JLayeredPane midPanelLeft = new JLayeredPane();
+                midPanelLeft.setBounds(0,100,600,600);
 
-        JPanel mplTop1 = GUI_Handler.createPanel(Color.cyan, 0, 0, 600, 25);
-        JPanel mplTop2 = GUI_Handler.createPanel(Color.yellow, 0, 25, 600, 75);
-        JPanel mplCenter = GUI_Handler.createPanel(Color.pink, 0, 100, 600, 400);
-        JPanel mplBottom = GUI_Handler.createPanel(Color.MAGENTA, 0, 500, 600, 100);
+                JPanel mplTop1 = GUI_Handler.createPanel(Color.cyan, 0, 0, 600, 25);
+                JPanel mplTop2 = GUI_Handler.createPanel(Color.yellow, 0, 25, 600, 50);
+                mplCenter = GUI_Handler.createPanel(Color.pink, 0, 75, 600, 425);
+                JPanel mplBottom = GUI_Handler.createPanel(Color.MAGENTA, 0, 500, 600, 100);
 
-        JLabel mplLabel = new JLabel("Usa il bottone per selezionare l'immagine da caricare:");
-        mplLabel.setBounds(0,0,350,10);
+                JLabel mplLabel = new JLabel("Usa il bottone per selezionare l'immagine da caricare:");
+                mplLabel.setBounds(0,0,350,10);
 
-        JButton mplButton = new JButton("Scegli foto");
-        mplButton.addActionListener(e -> uploadImage());
-        mplButton.setLayout(null);
-        mplButton.setBounds(10,20,100,100);
+                JButton mplButton = new JButton("Scegli foto");
+                mplButton.addActionListener(e -> {
+                    try {
+                        uploadImage();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+                mplButton.setLayout(null);
+                mplButton.setBounds(10,20,100,100);
 
-        BufferedImage buffImage = ImageIO.read(new File("C:\\Users\\samue\\OneDrive\\Desktop\\universita\\MAGISTRALE_1_anno\\Metodi_del_calcolo_scientifico\\progetto 2\\progetto2_metodiDelCaloloScientifico\\src\\main\\resources\\images\\20x20.bmp"));
-        Image image = buffImage.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
-        JLabel Jimage = new JLabel(new ImageIcon(image));
+                mplTop1.add(mplLabel);
+                mplTop2.add(mplButton);
 
-        mplTop1.add(mplLabel);
-        mplTop2.add(mplButton);
-        mplCenter.add(Jimage);
+                midPanelLeft.add(mplTop1);
+                midPanelLeft.add(mplTop2);
+                midPanelLeft.add(mplCenter);
+                midPanelLeft.add(mplBottom);
 
-        midPanelLeft.add(mplTop1);
-        midPanelLeft.add(mplTop2);
-        midPanelLeft.add(mplCenter);
-        midPanelLeft.add(mplBottom);
+                /*
+                 * Pannello centro destra
+                 */
+                JPanel midPanelRight = GUI_Handler.createPanel(Color.gray, 600,100,600,600);
 
-        /*
-         * Pannello centro destra
-        */
-        JPanel midPanelRight = GUI_Handler.createPanel(Color.gray, 600,100,600,600);
+                /*
+                 * Pannello inferiore
+                 */
+                JPanel bottomPanel = GUI_Handler.createPanel(Color.red, 0,700,1200,100);
 
-        /*
-         * Pannello inferiore
-        */
-        JPanel bottomPanel = GUI_Handler.createPanel(Color.red, 0,700,1200,100);
+                /*
+                 * Aggiunta elementi al frame principale
+                 */
+                frame.add(topPanel);
+                frame.add(midPanelLeft);
+                frame.add(midPanelRight);
+                frame.add(bottomPanel);
 
-        /*
-         * Aggiunta elementi al frame principale
-        */
-        frame.add(topPanel);
-        frame.add(midPanelLeft);
-        frame.add(midPanelRight);
-        frame.add(bottomPanel);
+            }
+        });
 
-        frame.revalidate();
     }
 
-    private static void uploadImage() {
+    private static void uploadImage() throws IOException {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setCurrentDirectory(new File("./src/main/resources/images"));
 
@@ -87,11 +97,22 @@ public class Main {
         int choice = fileChooser.showOpenDialog(null);
 
         if(choice == JFileChooser.APPROVE_OPTION) {
-            String imagePath = fileChooser.getSelectedFile().getAbsolutePath();
-            System.out.println(imagePath);
+            imagePath = fileChooser.getSelectedFile().getAbsolutePath();
+            BufferedImage buffImage = ImageIO.read(new File(imagePath));
+            Image image = buffImage.getScaledInstance(400, 400, Image.SCALE_SMOOTH);
+            JLabel Jimage = new JLabel(new ImageIcon(image));
+            Jimage.setVisible(true);
+            mplCenter.add(Jimage);
+            reload();
         } else {
             System.out.println("No image selected");
         }
+    }
+
+    private static void reload() {
+        frame.invalidate();
+        frame.validate();
+        frame.repaint();
     }
 
 }
