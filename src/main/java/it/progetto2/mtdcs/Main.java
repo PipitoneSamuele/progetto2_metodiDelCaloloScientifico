1d8ae4329a11a9288c9d66bcfaf287525c91dd28
PipitoneSamuele <samuele.pipitone@gmail.com>
2023-05-27 20:09:34 +0200

    dct2 funzionante da libreria

--- src/main/java/it/progetto2/mtdcs/Main.java
+++ src/main/java/it/progetto2/mtdcs/Main.java
@@ -1,6 +1,7 @@
 package it.progetto2.mtdcs;
 
 import edu.emory.mathcs.jtransforms.dct.DoubleDCT_1D;
+import edu.emory.mathcs.jtransforms.dct.DoubleDCT_2D;
 import it.progetto2.mtdcs.utility.Constants;
 import it.progetto2.mtdcs.utility.MatrixUtility;
 
@@ -9,13 +10,19 @@ public class Main {
     public static void main(String[] args) {
         //MainGUI gui = new MainGUI();
 
-        //TODO: IMPO, la dimensione di fdtc2 deve corrispondere alla dimensione della matrice a cui applicarla
-        DoubleDCT_1D fdct = new DoubleDCT_1D(8);
+        DoubleDCT_1D fdct = new DoubleDCT_1D(8); //La dimensione passata in input deve corrispondere con la dimensione del vettore
 
         MatrixUtility.printVector(Constants.TEST_VECTOR);
         fdct.forward(Constants.TEST_VECTOR, true);
         System.out.println("");
         MatrixUtility.printVector(Constants.TEST_VECTOR);
+
+        DoubleDCT_2D fdct2 = new DoubleDCT_2D(8,8);
+        MatrixUtility.printMatrix(Constants.TEST_MATRIX);
+        fdct2.forward(Constants.TEST_MATRIX, true);
+        System.out.println("");
+        MatrixUtility.printMatrix(Constants.TEST_MATRIX);
+
     }
 
 }
